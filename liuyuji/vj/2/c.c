ba436a96c4b7cf19f2ca4218f5de754957c94d81
HumbleHunger <2495970924@qq.com>
2020-02-10 22:11:22 +0800

    update

---
+++ liuyuji/vj/2/c.c
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月10日 星期一 16时36分20秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+#define min(a,b) ((a)<(b) ? (a):(b))
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(;t>0;t--){
+        int c,n;
+        scanf("%d %d",&n,&c);
+        int v[n+1];
+        int w[n+1];
+        v[0]=w[0]=0;
+        int dp[n+1][c+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++){
+            scanf("%d",&v[i]);
+        }
+        for(int i=1;i<=n;i++){
+            scanf("%d",&w[i]);
+        }
+        for(int i=1;i<=n;i++)
+	    {
+		    for(int j=1;j<=c;j++)
+		    {
+			    if(j<w[i])
+				    dp[i][j]=dp[i-1][j];
+			    else
+				    dp[i][j]=min(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);
+		    }
+	    }
+        if()printf("%d",dp[n][c]);
+    }
+    return 0;
+}
