d82725c55713268567ae9b805fdaff92ded69c60
HumbleHunger <2495970924@qq.com>
2020-02-11 16:09:20 +0800

    update

--- liuyuji/vj/2/c.c
+++ liuyuji/vj/2/c.c
@@ -13,30 +13,32 @@ int main()
     int t;
     scanf("%d",&t);
     for(;t>0;t--){
-        int c,n;
-        scanf("%d %d",&n,&c);
+        int c,n,a,b;
+        scanf("%d %d",&a,&b);
+        c=b-a;
+        scanf("%d",&n);
         int v[n+1];
         int w[n+1];
-        v[0]=w[0]=0;
-        int dp[n+1][c+1];
-        memset(dp,0,sizeof(dp));
+        int dp[c+1];
+        memset(dp,0x3f3f3f,sizeof(dp));
         for(int i=1;i<=n;i++){
-            scanf("%d",&v[i]);
+            scanf("%d %d",&v[i],&w[i]);
         }
-        for(int i=1;i<=n;i++){
-            scanf("%d",&w[i]);
-        }
+        v[0]=w[0]=dp[0]=0;
         for(int i=1;i<=n;i++)
-	    {
-		    for(int j=1;j<=c;j++)
-		    {
-			    if(j<w[i])
-				    dp[i][j]=dp[i-1][j];
-			    else
-				    dp[i][j]=min(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);
-		    }
-	    }
-        if()printf("%d",dp[n][c]);
+        {
+            for(int j=w[i];j<=c;j++)
+            {
+                dp[j]=min(dp[j],dp[j-w[i]]+v[i]);
+            }
+        }
+        if(dp[c]!=0x3f3f3f)
+        {
+            printf("The minimum amount of money in the piggy-bank is %d.\n",dp[c]);
+        }
+        else{
+            printf("This is impossible.\n");
+        }
     }
     return 0;
 }
