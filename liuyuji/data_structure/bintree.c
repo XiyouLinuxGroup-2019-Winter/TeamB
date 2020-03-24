11885061d648293a752a7f52888a5ec96d599b8a
HumbleHunger <2495970924@qq.com>
2020-03-24 21:52:28 +0800

    update

---
+++ liuyuji/data_structure/bintree.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: bintree.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年03月24日 星期二 21时39分17秒
+ ************************************************************************/
+//动态创建二叉树
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct node{
+    int date;
+    struct node *left;
+    struct node *right;
+}Node;
+Node *root;
+int main()
+{
+    
+}
