0bc4930437a54246df6f462e08313470ffc0c2e0
HumbleHunger <2495970924@qq.com>
2020-03-29 23:03:43 +0800

    update

--- liuyuji/C_ADT/Heap/Maxheap.c
+++ liuyuji/C_ADT/Heap/Maxheap.c
@@ -4,6 +4,19 @@
 	> Mail: 
 	> Created Time: 2020年03月28日 星期六 21时49分10秒
  ************************************************************************/
-
+//最大堆
 #include<stdio.h>
-
+#include<stdlib.h>
+typedef heap{
+    int *date;
+    int num;
+    int max;
+}Heap;
+int main()
+{
+    int num;
+    printf("输入堆的大小\n");
+    scanf("%d",&num);
+    creat(num);
+}
+Heap *creat(int max)
