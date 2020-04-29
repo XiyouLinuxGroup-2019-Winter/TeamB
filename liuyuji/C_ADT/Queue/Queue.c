6a6700d0babc3d183372a2f63d25cd051e8c5b06
HumbleHunger <2495970924@qq.com>
2020-04-29 22:11:10 +0800

    update

--- liuyuji/C_ADT/Queue/Queue.c
+++ liuyuji/C_ADT/Queue/Queue.c
@@ -16,70 +16,75 @@ typedef struct queue{
     Node *head;
     Node *tail;
     int num;
-    int len;
 }Queue;
-void look(Queue *p)
-{
-    if(p->head==NULL){
-        printf("队列为空\n");
-        return;
-    }
-    Node *operate=p->head;
-    printf("%d\n",operate->date);
-}
+Node *mark=NULL;
 int full(Queue *p)
 {
-    if(p->len==p->num){
+    Node *operate;
+    operate=p->tail;
+    if(operate==NULL){
+        return 0;
+    }
+    if(operate->next==NULL){
         return 1;
     }
     else{
         return 0;
     }
 }
-void create(Queue *p,int n)
+void create(int n,Queue *p)
 {
+    Node *operate,*record;
     p->num=n;
-    p->head=NULL;
+    operate=record=NULL;
+    for(int i=0;i<n;i++){
+        operate=(Node *)malloc(sizeof(Node));
+        if(i==0){
+            p->head=operate;
+            mark=operate;
+        }
+        else{
+            record->next=operate;
+        }
+        operate->date=0;
+        operate->next=NULL;
+        record=operate;
+    }
     p->tail=NULL;
-    p->len=0;
 }
-int insert(Queue *p,int buf)
+int insert(Queue *p,int n)
 {
     if(full(p)==1){
-        printf("队列已满\n");
         return 0;
     }
     Node *operate,*record;
-    operate=(Node *)malloc(sizeof(Node));
-    if(p->head==NULL){
-        p->head=operate;
+    if(p->tail==NULL){
+        operate=p->head;
     }
     else{
         record=p->tail;
-        record->next=operate;
+        operate=record->next;
     }
-    operate->date=buf;
+    operate->date=n;
     p->tail=operate;
-    p->len++;
-    return 0;
 }
 void rm(Queue *p)
 {
-    if(p->head==NULL){
-        printf("队列为空\n");
-        return;
-    }
     Node *operate=p->head;
     Node *record=operate->next;
     free(operate);
     p->head=record;
-    p->len--;
+}
+void look(Queue *p)
+{
+    Node *operate=p->head;
+    printf("%d\n",operate->date);
 }
 void del(Queue *p)
 {
     Node *operate,*record;
-    operate=p->head;
-    for(int i=0;i<p->len;i++){
+    operate=mark;
+    for(int i=0;i<p->num;i++){
         record=operate->next;
         free(operate);
         operate=record;
@@ -88,21 +93,17 @@ void del(Queue *p)
 int main()
 {
     printf("请输入队列长度\n");
-    int num;
-    scanf("%d",&num);
+    int len;
+    scanf("%d",&len);
     Queue q;
-    create(&q,num);
+    create(len,&q);
     while(1){
         printf("1.入队\n2.出队\n3.查看队头\n4.清空\n");
         int chose,a;
         scanf("%d",&chose);
         switch(chose){
             case 1:
-                while(1){
-                    if(full(&q)==1){
-                        printf("队列已满\n");
-                        break;
-                    }
+                while((full(&q))==0){
                     scanf("%d",&a);
                     insert(&q,a);
                     printf("继续？\n");
