ffc70506f419cefef040c767641249ca59ae64eb
HumbleHunger <2495970924@qq.com>
2020-05-15 22:19:00 +0800

    update

--- liuyuji/Linux_C/pc.c
+++ liuyuji/Linux_C/pc.c
@@ -21,7 +21,7 @@ typedef struct queue{
 }Queue;
 Queue pc;
 pthread_mutex_t mutex,mutex1,mutex2;
-pthread_cond_t cond1,cond2;
+pthread_cond_t cond,cond1,cond2;
 int full(Queue *p)
 {
     if(p->len==p->num){
@@ -42,7 +42,7 @@ int empty(Queue *p)
 }
 void create(Queue *p)
 {
-    p->num=10;
+    p->num=3;
     p->head=NULL;
     p->tail=NULL;
     p->len=0;
@@ -58,7 +58,7 @@ int insert(Queue *p)
         record=p->tail;
         record->next=operate;
     }
-    operate->date=0;
+    operate->date=rand()%50;
     p->tail=operate;
     p->len++;
     return 0;
@@ -84,19 +84,24 @@ void del(Queue *p)
 void *producer(void *a)
 {
     while(1){
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond,&mutex);
         if(full(&pc)==0){
-            pthread_mutex_lock(&mutex);
             insert(&pc);
-            pthread_mutex_unlock(&mutex);
-            printf("生产者生产一个,当前产品数为%d\n",pc.len);
+            printf("生产者生产%d\n",pc.tail->date);
             if(pc.len==1){
                 pthread_cond_signal(&cond2);
             }
+            pthread_mutex_unlock(&mutex);
             sleep(1);
+            pthread_cond_broadcast(&cond);
         }
         else{
             pthread_mutex_lock(&mutex1);
             printf("生产者挂起\n");
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
+            pthread_cond_broadcast(&cond);
             pthread_cond_wait(&cond1,&mutex1);
             printf("生产者运行\n");
             pthread_mutex_unlock(&mutex1);
@@ -106,42 +111,66 @@ void *producer(void *a)
 void *consumer(void *a)
 {
     while(1){
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond,&mutex);
         if(empty(&pc)==0){
-            pthread_mutex_lock(&mutex);
+            printf("\t\t\t消费者消费%d\n",pc.head->date);
             rm(&pc);
-            pthread_mutex_unlock(&mutex);
-            printf("消费者消费一个,当前产品数为%d\n",pc.len);
             if(pc.len==9){
                 pthread_cond_signal(&cond1);
             }
+            pthread_mutex_unlock(&mutex);
             sleep(1);
+            pthread_cond_broadcast(&cond);
         }
         else{
             pthread_mutex_lock(&mutex2);
-            printf("消费者挂起\n");
+            printf("\t\t\t消费者挂起\n");
+            pthread_mutex_unlock(&mutex);
+            sleep(1);
+            pthread_cond_broadcast(&cond);
             pthread_cond_wait(&cond2,&mutex2);
-            printf("消费者运行\n");
+            printf("\t\t\t消费者运行\n");
             pthread_mutex_unlock(&mutex2);
         }
     }
 }
 int main()
 {
-    pthread_t th1,th2;
+    pthread_t th1,th2,th3,th4,th5,th6;
     pthread_mutex_init(&mutex1,NULL);
+    pthread_cond_init(&cond,NULL);
     pthread_cond_init(&cond1,NULL);
     pthread_mutex_init(&mutex2,NULL);
     pthread_mutex_init(&mutex,NULL);
     pthread_cond_init(&cond2,NULL);
-    printf("生产者数目1\n消费者数目1\n");
     create(&pc);
-    int ret=pthread_create(&th1,NULL,producer,NULL);
+    int ret;
+    ret=pthread_create(&th1,NULL,producer,NULL);
     if(ret!=0)
         printf("pthread_create produser error ");
-    ret=pthread_create(&th2,NULL,consumer,NULL);
+    ret=pthread_create(&th2,NULL,producer,NULL);
+    if(ret!=0)
+        printf("pthread_create produser error ");
+    ret=pthread_create(&th3,NULL,producer,NULL);
+    if(ret!=0)
+        printf("pthread_create produser error ");
+    ret=pthread_create(&th4,NULL,consumer,NULL);
     if(ret!=0)
         printf("pthread_create consumer error ");
-    sleep(20);
+    ret=pthread_create(&th5,NULL,consumer,NULL);
+    if(ret!=0)
+        printf("pthread_create consumer error ");
+    ret=pthread_create(&th6,NULL,consumer,NULL);
+    if(ret!=0)
+        printf("pthread_create consumer error ");
+    pthread_cond_signal(&cond);
+    pthread_join(th1,NULL);
+    pthread_join(th2,NULL);
+    pthread_join(th3,NULL);
+    pthread_join(th4,NULL);
+    pthread_join(th5,NULL);
+    pthread_join(th6,NULL);
     del(&pc);
     printf("all over\n");
     exit(0);
